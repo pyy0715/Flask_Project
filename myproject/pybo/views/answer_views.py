23189bdb858d3cf1d3ae93da0ecd017698c6e69b
pyy0715 <zxc077@naver.com>
2020-09-01 22:53:36 +0900

    add Recommendation

--- myproject/pybo/views/answer_views.py
+++ myproject/pybo/views/answer_views.py
@@ -21,7 +21,8 @@ def create(question_id):
         answer = Answer(content=content, create_date=datetime.now(), user=g.user)
         question.answer_set.append(answer)
         db.session.commit()
-        return redirect(url_for('question.detail', question_id=question_id))
+        return redirect('{}#answer_{}'.format(
+            url_for('question.detail', question_id=question_id), answer.id))
     return render_template('question/quesion_datail.html', question=question, form=form)
 
 @bp.route('/modify/<int:answer_id>', methods=('GET', 'POST'))
@@ -37,7 +38,8 @@ def modify(answer_id):
             form.populate_obj(answer)
             answer.modify_date = datetime.now()  # 수정일시 저장
             db.session.commit()
-            return redirect(url_for('question.detail', question_id=answer.question.id))
+            return redirect('{}#answer_{}'.format(
+                url_for('question.detail', question_id=answer.question.id), answer.id))
     else:
         form = AnswerForm(obj=answer)
     return render_template('answer/answer_form.html', answer=answer, form=form)
